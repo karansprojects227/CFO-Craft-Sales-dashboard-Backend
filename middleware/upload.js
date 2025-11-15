1943b7fe7c80d9eda8c42ad70ec2208c3ec50e23
Karan Kumar <karansprojects227@gmail.com>
2025-11-15 19:29:40 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,14 +1,7 @@
 const multer = require("multer");
-const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
+// Store file in memory (buffer)
+const storage = multer.memoryStorage();
 
 // Allow only JPG/PNG
 function fileFilter(req, file, cb) {
@@ -24,6 +17,7 @@ function fileFilter(req, file, cb) {
 const upload = multer({
   storage,
   fileFilter,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
 });
 
 module.exports = { upload };
