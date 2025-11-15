93e628f58e506dc7619577320c6d19accf08bd31
Karan Kumar <karansprojects227@gmail.com>
2025-11-15 18:34:01 +0530

    Delete routes/userRoutes.js

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const checkUserExist = require("../middleware/checkUserExist");
-const router = express.Router();
-
-router.get("/checkUserExist", checkUserExist, (req, res) => {
-  return res.json({ success: true });  // user hai
-});
-
-module.exports = router;
