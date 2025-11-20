1197918d527b8cedd77c0cedca904e0f4842e50c
Karan Kumar <karansprojects227@gmail.com>
2025-11-20 17:29:02 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -3,13 +3,43 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePic: { type: String, default:"default-user" },
+
+    phone: {
+      type: String,
+      required: function () {
+        return !this.googleId; // Google login ke liye optional
+      },
+      select: false,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: function () {
+        return !this.googleId; // Google login ke liye optional
+      },
+      select: false,
+    },
+
+    googleId: {
+      type: String,
+      default: null,
+    },
+
+    profilePic: {
+      type: String,
+      default: "default-user",
+      select: true,   // ← ADD THIS LINE
+    },
   },
   { timestamps: true }
 );
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
-
 module.exports = User;
