32b9dceb3bbe202aecae8bc8fd54fbaa10739d7f
Karan Kumar <karansprojects227@gmail.com>
2025-11-20 20:06:48 +0530

    Update googleStrategy.js

--- controllers/googleStrategy.js
+++ controllers/googleStrategy.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL: `${process.env.FRONTEND_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -28,3 +28,4 @@ passport.use(
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
+
