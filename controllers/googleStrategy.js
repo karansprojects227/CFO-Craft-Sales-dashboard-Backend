950cd60a6bfc2b35f5439c49e57c7892df365f9a
Karan Kumar <karansprojects227@gmail.com>
2025-11-21 18:07:48 +0530

    Update googleStrategy.js

--- controllers/googleStrategy.js
+++ controllers/googleStrategy.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `https://cfo-craft-sales-dashboard-backend.onrender.com/api/auth/google/callback`,
+      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -28,4 +28,5 @@ passport.use(
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
+
 
