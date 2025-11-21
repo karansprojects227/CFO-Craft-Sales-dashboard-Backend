2837caf744957cf87d2f25bd35eef69d33ffa6b0
Karan Kumar <karansprojects227@gmail.com>
2025-11-21 16:22:47 +0530

    Add files via upload

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,8 @@
-const Redis = require('ioredis');
+const Redis = require("ioredis");
 
-const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
-  tls: { rejectUnauthorized: false } // Upstash TLS
-});
+const redis = new Redis("redis://127.0.0.1:6379");
 
-redis.on('connect', () => console.log('Redis connected'));
-redis.on('error', (err) => console.error('Redis error', err));
+redis.on("connect", () => console.log("Redis connected (Localhost)"));
+redis.on("error", (err) => console.error("Redis error:", err));
 
 module.exports = redis;
