eb3634ce1f3da9a8e00c970806d1c71fba098e14
Karan Kumar <karansprojects227@gmail.com>
2025-11-22 19:53:58 +0530

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -1,16 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 
 module.exports = transporter;
+
