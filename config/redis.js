8bd46e33e09a839c4a3fa29223d3d80aa74a67c6
Karan Kumar <karansprojects227@gmail.com>
2025-11-22 18:36:25 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -4,8 +4,9 @@ const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
   tls: { rejectUnauthorized: false } // Upstash TLS
 });
 
-redis.on("connect", () => console.log("Redis connected (Localhost)"));
+redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error("Redis error:", err));
 
 
 module.exports = redis;
+
