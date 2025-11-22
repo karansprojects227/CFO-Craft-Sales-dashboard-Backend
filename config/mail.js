e80221c9ee4f0251834be49c14d51a1d45dde787
Karan Kumar <karansprojects227@gmail.com>
2025-11-22 17:49:54 +0530

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -3,9 +3,11 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true, // SSL
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // APP PASSWORD
+    pass: process.env.EMAIL_PASS,
   },
 });
+
+module.exports = transporter;
